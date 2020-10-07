0247ba6f06faf0af8d1d4bd427ab48943e0da1d6
JUAN MANUEL VERDEJO UTRILLA <JuanManuel.Verdejo@alu.uclm.es>
2020-10-07 19:58:11 +0200

    Unir a partida

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -12,34 +12,74 @@ function Juego(){
 
 	}
 	
-	this.unirPartida() = function(){
+	this.unirAPartida = function(codigo, nick){
+		if(this.partidas[codigo]){
+			let partida = this.partidas[codigo];
+			partida.agregarUsuario(nick);
+		}
+
 	}
 	
 	this.obtenerCodigo=function(){
 		let cadena="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		let letras = cadena.split('');
+		let maxCadena = cadena.length;
 		let codigo = [];
 		for(i=0;i<6;i++){
-			codigo.push(letras[randomInt(1,25)-1]);
+			codigo.push(letras[randomInt(1,maxCadena)-1]);
 		}
 		return codigo.join('');
 	}
 }
 
 function Partida(num, owner){
-	this.numUusuarios = num;
-	this.owner = owner;
-	this.usuarios = []; //el index 0 es owner
-	//this.usuarios = {} //version array asociativo
+	this.numUsuarios = num;
+	this.nickOwner = owner;
+	this.fase = new Inicial();
+	this.agregarUsuario
+	//this.usuarios = []; //el index 0 es owner
+	this.usuarios = {} //version array asociativo
 	this.agregarUsuario=function(nick){
+		this.fase.agregarUsuario(nick,this);
+	}
+	this.puedeAgregarUsuario=function(nick){
 		//comprobar que el nick es unico
 		//comprobar si el usuario num (maximo)
-		
+		if(this.numUsuarios > Object.keys(this.usuarios).length){
+			let nuevo = nick;
+			let contador = 1;
+			while(this.usuarios[nuevo]){
+				nuevo = nick + contador;
+				contador = contador + 1;
+			}
+			this.usuarios[nuevo] = new Usuario(nuevo);
+		}else{
+			console.log("Numero de jugadores máximo alcanzado");
+		}
 	}
 	
 	this.agregarUsuario(owner);
 }
 
+function Usuario(nick){
+	this.nick=nick;
+}
+
+function Inicial(){{}
+	this.agregarUsuario=function(nick,partida){
+		partida.puedeAgregarUsuario(nick);
+	}
+}
+function Jugando(){
+	this.agregarUsuario=function(nick,juego){
+		//this.puedeAgregarUsuario(nick);
+	}
+}
+function Final(){
+	this.agregarUsuario=function(nick,juego){
+		//this.puedeAgregarUsuario(nick);
+	}
+}
 function randomInt(low, high) {
 	return Math.floor(Math.random() * (high - low) + low);
 }
