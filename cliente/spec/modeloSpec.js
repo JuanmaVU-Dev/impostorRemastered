722d57779fe225feac62ba272cb60ea2d1b3961d
JUAN MANUEL VERDEJO UTRILLA <JuanManuel.Verdejo@alu.uclm.es>
2020-10-21 13:57:32 +0200

    Pruebas: asignar impostor, asignar tareas y abandonar partida

--- cliente/spec/modeloSpec.js
+++ cliente/spec/modeloSpec.js
@@ -38,7 +38,17 @@ describe("El juego del impostor", function () {
             expect(juego.partidas[codigo].fase.nombre).toEqual("Completado");
         });
 
-        it("pepe iniciar la partida", function () {
+        it("No se ha asignado ningún impostor",function(){
+            var partida = juego.partidas[codigo];
+            var nimpostores = 0;
+            var keys = Object.keys(juego.partidas[codigo].usuarios);
+            for(i=0;i<keys.length;i++){
+                if(partida.usuarios[keys[i]].impostor) nimpostores++;
+            }
+            expect(nimpostores).toEqual(0);
+        })
+
+        it("pepe inicia la partida", function () {
             juego.unirAPartida(codigo, "María");
             juego.unirAPartida(codigo, "José Carlos");
             juego.unirAPartida(codigo, "Mario");
@@ -47,5 +57,41 @@ describe("El juego del impostor", function () {
             usuario.iniciarPartida();
             expect(juego.partidas[codigo].fase.nombre).toEqual("Jugando");
         });
+
+
+        describe("La partida ya esta iniciada", function () {
+            var partida;
+
+            beforeEach(function () {
+                juego.unirAPartida(codigo, "María");
+                juego.unirAPartida(codigo, "José Carlos");
+                juego.unirAPartida(codigo, "Mario");
+                usuario.iniciarPartida();
+                partida = juego.partidas[codigo];
+            });
+
+            it("Se ha asignado un impostor",function(){
+                var nimpostores = 0;
+                var keys = Object.keys(juego.partidas[codigo].usuarios);
+                for(i=0;i<keys.length;i++){
+                    if(partida.usuarios[keys[i]].impostor) nimpostores++;
+                }
+                expect(nimpostores).toEqual(1);
+            })
+            it("Se han asignado las tareas",function(){
+                var keys = Object.keys(juego.partidas[codigo].usuarios);
+                for(i=0;i<keys.length;i++){
+                    expect(partida.usuarios[keys[i]].encargo).not.toEqual("ninguno");
+                }
+            })
+            it("Mario sale de la partida", function () {
+                expect(Object.keys(partida.usuarios).length).toEqual(4);
+                var mario = partida.usuarios['Mario'];
+                mario.abandonarPartida();
+                expect(Object.keys(partida.usuarios).length).toEqual(3);
+                expect(partida.usuarios['Mario']).toBe(undefined);
+            });
+        });
+
     });
 });
